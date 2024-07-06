c61002024ddb02908d99a272bf3033c790e57025
SangJin Han <s202188@naver.com>
2024-07-07 01:13:27 +0900

    create demo app

---
+++ HaviImageProviderDemo/HaviImageProviderDemo/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  HaviImageProviderDemo
+//
+//  Created by 한상진 on 7/7/24.
+//
+
+import SwiftUI
+import HaviImageProvider
+
+struct ContentView: View {
+  var body: some View {
+    Text("hi")
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ HaviImageProviderDemo/HaviImageProviderDemo/HaviImageProviderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaviImageProviderDemoApp.swift
+//  HaviImageProviderDemo
+//
+//  Created by 한상진 on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HaviImageProviderDemoApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
